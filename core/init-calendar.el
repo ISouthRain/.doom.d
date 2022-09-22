b3dfd9ef0cbefb4b347cca0f060043277b9de3fa
ISouthRain <isouthrain@gmail.com>
2022-09-22 23:02:36 +0800

    pyim update

--- core/init-calendar.el
+++ core/init-calendar.el
@@ -39,22 +39,26 @@
   (setq calendar-mark-holidays-flag t
         cal-china-x-important-holidays cal-china-x-chinese-holidays
         cal-china-x-general-holidays '((holiday-lunar 1 15 "元宵节")
-                                       (holiday-lunar 7 7 "七夕节")
-                                       (holiday-lunar 8 15 "中秋节")
                                        (holiday-fixed 3 8 "妇女节")
                                        (holiday-fixed 3 12 "植树节")
                                        (holiday-fixed 5 4 "青年节")
                                        (holiday-fixed 6 1 "儿童节")
+                                       (holiday-lunar 7 7 "七夕节")
+                                       (holiday-lunar 8 15 "中秋节")
                                        (holiday-fixed 9 10 "教师节")
                                        (holiday-fixed 10 1 "国庆节")
                                        )
         holiday-other-holidays '((holiday-fixed 2 14 "情人节")
                                  (holiday-fixed 4 1 "愚人节")
+                                 (holiday-fixed 9 1 "全国开学日")
                                  (holiday-fixed 12 25 "圣诞节")
                                  (holiday-float 5 0 2 "母亲节")
                                  (holiday-float 6 0 3 "父亲节")
-                                 (holiday-float 11 4 4 "感恩节"))
-        holiday-custom-holidays '((holiday-lunar 7 29 "Happy Birthday"))
+                                 (holiday-float 11 4 4 "感恩节")
+                                 )
+        holiday-custom-holidays '((holiday-lunar 7 29 "Happy Birthday")
+                                  (holiday-lunar 2 3 "纪念奶奶")
+                                  )
         calendar-holidays (append cal-china-x-important-holidays
                                   cal-china-x-general-holidays
                                   holiday-other-holidays
