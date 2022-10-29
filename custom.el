0941663d242d17d636e4a18696d84a9d2d2979ae
ISouthRain <isouthrain@gmail.com>
2022-10-29 21:45:11 +0800

    Update README.org

--- custom.el
+++ custom.el
@@ -1,3 +1,15 @@
 (put 'projectile-ag 'disabled nil)
 (put 'projectile-grep 'disabled nil)
 (put 'projectile-ripgrep 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
